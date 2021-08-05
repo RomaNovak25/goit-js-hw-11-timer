4f6559f827a6e000a09190d1d6a52dce39ebd08d
RomaNovak25 <roma.novak.19966@gmail.com>
2021-08-06 01:57:44 +0300

    changed files

--- index.js
+++ index.js
@@ -4,12 +4,59 @@ const timer = {
 
     setInterval(() => {
       const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const timerComponents = timerFn(deltaTime);
 
-      console.log(currentTime - startTime);
+      // console.log(startTime - currentTime);
     }, 1000);
   },
 };
 
 timer.start();
 
-console.log('qweqwe');
+// const PLANE_TIME =
+
+/*
+ * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+ * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+ */
+// const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+/*
+ * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+ * остатка % и делим его на количество миллисекунд в одном часе
+ * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+ */
+// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+/*
+ * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+ * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+ */
+// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+
+/*
+ * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+ * миллисекунд в одной секунде (1000)
+ */
+// const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+function timerFn(time) {
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+
+  const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+  return { days, hours, mins, secs };
+}
+
+console.log(timerFn(11,22,1,1));
+
+setTimeout(() => {
+  
+}, 1000);
+
+timerFn();
